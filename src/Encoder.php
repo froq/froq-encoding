b1dcc868b6275b3eb88cc97b57da3debe7d55835
Kerem Güneş <qeremy@gmail.com>
2020-12-23 06:46:14 +0300

    Head doc-ups, doc-ups.

--- src/Encoder.php
+++ src/Encoder.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Copyright (c) 2015 · Kerem Güneş
- * Apache License 2.0 <https://opensource.org/licenses/apache-2.0>
+ * Apache License 2.0 · http://github.com/froq/froq-encoding
  */
 declare(strict_types=1);
 
@@ -18,7 +18,7 @@ use Throwable;
  *
  * @package froq\encoding
  * @object  froq\encoding\Encoder
- * @author  Kerem Güneş <k-gun@mail.com>
+ * @author  Kerem Güneş
  * @since   4.0
  * @static
  */
