439642158c2825bb0b6687c427dcb724d0c360bc
Kerem Güneş <qeremy@gmail.com>
2022-03-15 23:50:42 +0300

    * Encoding:     - extend Enum.     - add more encodings.

--- src/Encoding.php
+++ src/Encoding.php
@@ -7,6 +7,8 @@ declare(strict_types=1);
 
 namespace froq\encoding;
 
+use froq\common\object\Enum;
+
 /**
  * Encoding.
  *
@@ -15,12 +17,40 @@ namespace froq\encoding;
  * @author  Kerem Güneş
  * @since   6.0
  */
-class Encoding extends \StaticClass
+class Encoding extends Enum
 {
     /**
-     * Types.
+     * ASCII.
      * @const string
      */
-    final const TYPE_UTF_8 = 'UTF-8',
-                TYPE_ASCII = 'ASCII',
+    public final const ASCII = 'ascii';
+
+    /**
+     * UTF set.
+     * @const string
+     */
+    public final const UTF_8 = 'utf-8',
+                       UTF_16 = 'utf-16',
+                       UTF_32 = 'utf-32';
+
+    /**
+     * ISO set.
+     * @const string
+     */
+    public final const ISO_8859_1 = 'iso-8859-1',
+                       ISO_8859_2 = 'iso-8859-2',
+                       ISO_8859_3 = 'iso-8859-3',
+                       ISO_8859_4 = 'iso-8859-4',
+                       ISO_8859_5 = 'iso-8859-5',
+                       ISO_8859_6 = 'iso-8859-6',
+                       ISO_8859_7 = 'iso-8859-7',
+                       ISO_8859_8 = 'iso-8859-8',
+                       ISO_8859_9 = 'iso-8859-9',
+                       ISO_8859_10 = 'iso-8859-10',
+                       ISO_8859_11 = 'iso-8859-11',
+                       ISO_8859_12 = 'iso-8859-12',
+                       ISO_8859_13 = 'iso-8859-13',
+                       ISO_8859_14 = 'iso-8859-14',
+                       ISO_8859_15 = 'iso-8859-15',
+                       ISO_8859_16 = 'iso-8859-16';
 }
