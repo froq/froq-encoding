30a38ae599a64f768b17b0d8e08853b1a223f116
Kerem Güneş <qeremy@gmail.com>
2022-03-20 13:29:18 +0300

    Add EncodingError.

---
+++ src/EncodingError.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright (c) 2015 · Kerem Güneş
+ * Apache License 2.0 · http://github.com/froq/froq-encoding
+ */
+declare(strict_types=1);
+
+namespace froq\encoding;
+
+/**
+ * @package froq\encoding
+ * @object  froq\encoding\EncodingError
+ * @author  Kerem Güneş
+ * @since   6.0
+ */
+class EncodingError extends \froq\common\Error
+{}
